9e3d548ba39c533794562dfe1b7e36e43e51c3e6
ashish <hrushikeshmohanty90@gmail.com>
2020-04-03 20:02:49 +0530

    first commit

---
+++ Assets/Scripts/Obstaclescripts/ObstacleSpawner.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obstaclePrefab;
+
+    [SerializeField]
+    private float obstacleHeight = 0.5f, obstacleWidth = 2f;
+    int index = 0;
+
+    [SerializeField]
+    int minX = -5, maxX = 6;
+
+    public static ObstacleSpawner instance = null;
+
+    List<GameObject> obstacleList = new List<GameObject>();
+
+
+    void Start()
+    {
+        AddObstaclesToList();
+
+        if (instance == null) {
+            instance = this;
+        }
+
+        for(int i = 0; i < 5; i++) {
+            CreateObstacle();
+        }
+    }
+
+    void AddObstaclesToList() {
+        for (int i = 0; i < 5; i++) {
+            GameObject obstacleToAdd = Instantiate(obstaclePrefab);
+            obstacleToAdd.SetActive(false);
+            obstacleList.Add(obstacleToAdd);
+        }
+    }
+
+
+    GameObject GetObstacle() {
+        GameObject obstacleToBeActive;
+        for (int i = 0; i < obstacleList.Count; i++) {
+            if (!obstacleList[i].activeInHierarchy) {
+                obstacleToBeActive = obstacleList[i];
+                return obstacleToBeActive;
+            }
+        }
+        return null;
+    }
+
+    public void CreateObstacle() {
+        int randomPositionX;
+        if (index == 0) {
+            randomPositionX = 0;
+        }
+        else {
+            randomPositionX = Random.Range(minX, maxX);
+        }
+        Vector2 newPosition = new Vector2(randomPositionX, index * 5);
+
+        //GameObject obstacle = Instantiate(obstaclePrefab, newPosition, Quaternion.identity);
+        //Instead of creating many obstacles and delting, create 5 in a list, set them active and deactivate them accordingly
+
+        GameObject obstacle = GetObstacle();
+        obstacle.SetActive(true);
+        obstacle.transform.position = newPosition;
+        obstacle.transform.rotation = Quaternion.identity;        
+        obstacle.transform.SetParent(transform);
+        obstacle.transform.localScale = new Vector2(obstacleWidth, obstacleHeight);
+        index++;
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    float jumpForce;
+    public float grav = 1f;
+    public float jumpHeight = 10f;
+    bool isDragging = false;
+    Vector2 mouseStartPos, playerPos, mousePos;
+    public float leftBound, rightBound;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+
+            if (rb.velocity.y <= 0)
+            {
+                jumpForce = grav * jumpHeight;
+                rb.velocity = new Vector2(0f, jumpForce);
+                DestroyAndCreate(other);
+            }
+
+        }
+    }
+
+    void DestroyAndCreate(Collider2D obstacle) {
+        //Destroy(obstacle.gameObject);         //This would take up a lot of cpu power
+        obstacle.gameObject.SetActive(false);
+        ObstacleSpawner.instance.CreateObstacle();
+    }
+
+
+    void AdddGravity()
+    {
+        rb.velocity = new Vector2(0, rb.velocity.y - (grav * grav));
+    }
+
+    void Update()
+    {
+        AdddGravity();
+        GetInput();
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (isDragging)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            transform.position = new Vector2(playerPos.x + (mousePos.x - mouseStartPos.x), transform.position.y);
+
+        }
+
+        if (transform.position.x >= rightBound)
+        {
+            transform.position = new Vector2(rightBound, transform.position.y);
+        }
+
+        if (transform.position.x <= leftBound)
+        {
+            transform.position = new Vector2(leftBound, transform.position.y);
+        }
+
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+            mouseStartPos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            playerPos = transform.position;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+    }
+}
